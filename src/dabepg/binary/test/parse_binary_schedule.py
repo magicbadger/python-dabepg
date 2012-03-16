4bc625f5099e269f38188cf7997e08a966d2195f
magicbadger@gmail.com <magicbadger@gmail.com@df4bd73a-ee04-11dd-a45c-2d54f07c7ce3>
2012-03-16 09:03:15 +0000

    git-svn-id: https://dab-epg.googlecode.com/svn/python/trunk@326 df4bd73a-ee04-11dd-a45c-2d54f07c7ce3

--- src/dabepg/binary/test/parse_binary_schedule.py
+++ src/dabepg/binary/test/parse_binary_schedule.py
@@ -1,8 +1,25 @@
 from dabepg.binary import unmarshall
 import logging
+import sys
 
+import logging
 logging.basicConfig(level=logging.DEBUG)
+logging.getLogger('dabepg').setLevel(logging.INFO)
 
+def usage():
+    print "USAGE: parse_binary_schedule.py [filename] (or expects stdin)"""
 
-epg = unmarshall(open('epg.dat', 'rb').read())
-print epg
+args = sys.argv[1:]
+if len(args):
+    filename = args[0]
+    print 'decoding from', filename
+    f = open(filename, 'rb')
+else:
+    f = sys.stdin
+
+epg = unmarshall(f)
+print 'EPG, scope:', epg.schedule.get_scope()
+print
+for programme in epg.schedule.programmes:
+    print programme.get_name(), ['%s/%s' % (x[0].isoformat(), (x[0] + x[1]).isoformat()) for x in programme.get_times()]
+    print '\t', programme.get_description()
